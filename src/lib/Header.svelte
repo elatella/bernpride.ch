b320621193af2d395cc3b6a9cc7200470b0d9ca1
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2022-05-22 16:45:51 +0200

    Initial commit

---
+++ src/lib/Header.svelte
@@ -0,0 +1,11 @@
+<section class="root">
+	<h1>Bern kriegt eine Pride!</h1>
+</section>
+
+<style>
+	.root {
+		text-align: center;
+		background-color: #ff0048;
+		color: #ffffa6;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import Header from '$lib/Header.svelte';
+	import Countdown from '$lib/Countdown.svelte';
+</script>
+
+<svelte:head>
+	<title>BernPride 2023</title>
+	<meta
+		name="description"
+		content="Bern kriegt eine Pride. Am 29.7.2023 findet in Bern wieder eine Pride Parade statt."
+	/>
+</svelte:head>
+
+<Header />
+
+<Countdown />
+
+<section style="background-color:#99edd6;color:#ff0048">
+	<h2>BERN WIRD BUNT</h2>
+	<h3>AM 29.07.2023 IST ES SOWEIT</h3>
+	<p>
+		Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad
+		nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+		Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla
+		est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+		reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa
+		proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
+		reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat
+		ullamco ut ea consectetur et est culpa et culpa duis.
+	</p>
+	<button>Newsletter abonnieren</button>
+</section>
+
+<section style="background-color:#ff0048;color:#ffffa6">
+	<h2>2023 WIRD EINZIGARTIG</h2>
+</section>
+
+<section style="background-color:#ffffff;color:#ff0048">
+	UP TO DATE BLEIBEN
+	<div>
+		<button>Newsletter abonnieren</button>
+	</div>
+	<div>
+		<button>Newsletter abonnieren</button>
+	</div>
+	<div>
+		<button>Newsletter abonnieren</button>
+	</div>
+</section>
+
+<section style="background-color:#d6e3ff;color:#ff0048">FAQ</section>
+
+<footer style="color:#ff0048">
+	<div>Verein EuroGames Bern</div>
+</footer>
