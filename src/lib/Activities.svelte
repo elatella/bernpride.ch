3fda405b461d439a3c45b35f407a1ebf2ab914ab
Raphaela Seeger <elatella@users.noreply.github.com>
2022-08-01 00:30:27 +0200

    Edit Activities, implement links

--- src/lib/Activities.svelte
+++ src/lib/Activities.svelte
@@ -7,10 +7,32 @@
 <section id="activities" class="root">
 	<div class="main-content">
 		<h2>2023 WIRD EINZIGARTIG</h2>
-		<div>
-			<img src={rainbow} alt="PrideRainbow" height="100" />
-			<img src={shoe} alt="PrideRun" height="100" />
-			<img src={eurogames} alt="EuroGames" height="100" />
+		<p>
+			Die BernPride ist nicht das einzige Highlight des Jahres. Schon ab dem 26. Juli 2023 wird es
+			in Bern bunt und queer. Wir begrüssen dich dann zu den EuroGames, der grössten queeren
+			Sportveranstaltung Europas, bieten dir am PrideRun die Möglichkeit, deine Marathon-Qualitäten
+			unter Beweis zu stellen und runden das Erlebnis mit der Pride am 29. Juli ab. Mehr
+			Informationen findest du mit nur einem Klick auf die Events!
+		</p>
+		<div class="links">
+			<div class="link">
+				<a href="/" target="_blank" class="activity-icon">
+					<img src={rainbow} alt="PrideRainbow" height="100" />
+				</a>
+				<span>BernPride</span>
+			</div>
+			<div class="link">
+				<a href="https://eurogames2023.ch/de/pride-run/" target="_blank" class="activity-icon">
+					<img src={shoe} alt="PrideRun" height="100" />
+				</a>
+				<span>PrideRun</span>
+			</div>
+			<div class="link">
+				<a href="https://eurogames2023.ch" target="_blank" class="activity-icon">
+					<img src={eurogames} alt="EuroGames" height="100" />
+				</a>
+				<span>EuroGames</span>
+			</div>
 		</div>
 	</div>
 </section>
@@ -25,5 +47,19 @@
 
 	.main-content {
 		flex-direction: column;
+	}
+
+	.links {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+		margin-top: 30px;
+		margin-bottom: 50px;
+	}
+
+	.link {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
 	}
 </style>
