1401a31b1ab91a78819cb537caa7d113465d164b
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2022-07-31 16:51:07 +0200

    Don't use SPA mode

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,10 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		})
+		adapter: adapter(),
+		prerender: {
+			default: true
+		}
 	}
 };
 
