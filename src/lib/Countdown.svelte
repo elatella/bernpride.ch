a2698ef831a9f38d27d4762296406a1ba79832b5
Raphaela Seeger <elatella@users.noreply.github.com>
2022-07-31 23:26:15 +0200

    Implement Countdown

--- src/lib/Countdown.svelte
+++ src/lib/Countdown.svelte
@@ -1,5 +1,49 @@
 <script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
 	import clock from '$lib/assets/clock.svg';
+
+	const SECOND = 1000;
+	const MINUTE = 60 * SECOND;
+	const HOUR = 60 * MINUTE;
+	const DAY = 24 * HOUR;
+	const MONTH = 30.4167 * DAY;
+
+	let timer: NodeJS.Timer | null = null;
+
+	const start = new Date(2023, 7, 29, 12);
+	let now = new Date();
+
+	let monthCount = 0;
+	let dayCount = 0;
+	let hourCount = 0;
+	let minuteCount = 0;
+	let secondCount = 0;
+
+	$: {
+		let timeDiff = start.getTime() - now.getTime();
+
+		monthCount = Math.floor(timeDiff / MONTH);
+		timeDiff = timeDiff % MONTH;
+		dayCount = Math.floor(timeDiff / DAY);
+		timeDiff = timeDiff % DAY;
+		hourCount = Math.floor(timeDiff / HOUR);
+		timeDiff = timeDiff % HOUR;
+		minuteCount = Math.floor(timeDiff / MINUTE);
+		timeDiff = timeDiff % MINUTE;
+		secondCount = Math.floor(timeDiff / SECOND);
+	}
+
+	onMount(() => {
+		timer = setInterval(() => {
+			now = new Date();
+		}, SECOND);
+	});
+
+	onDestroy(() => {
+		if (timer) {
+			clearInterval(timer);
+		}
+	});
 </script>
 
 <section id="countdown" class="root">
@@ -10,11 +54,11 @@
 		</div>
 
 		<div class="clock">
-			<div class="clock-amount"><span>13</span><span>Monate</span></div>
-			<div class="clock-amount"><span>25</span><span>Tage</span></div>
-			<div class="clock-amount"><span>7</span><span>Stunden</span></div>
-			<div class="clock-amount"><span>35</span><span>Minuten</span></div>
-			<div class="clock-amount"><span>5</span><span>Sekunden</span></div>
+			<div class="clock-amount"><span>{monthCount}</span><span>Monate</span></div>
+			<div class="clock-amount"><span>{dayCount}</span><span>Tage</span></div>
+			<div class="clock-amount"><span>{hourCount}</span><span>Stunden</span></div>
+			<div class="clock-amount"><span>{minuteCount}</span><span>Minuten</span></div>
+			<div class="clock-amount"><span>{secondCount}</span><span>Sekunden</span></div>
 		</div>
 	</div>
 </section>
