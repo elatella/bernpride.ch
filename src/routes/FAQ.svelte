ad371814d512670b95fec9fcf375425034b4d0b6
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2022-12-13 10:05:45 +0100

    Add link to info mail

--- src/routes/FAQ.svelte
+++ src/routes/FAQ.svelte
@@ -26,11 +26,11 @@
 		},
 		{
 			title: 'Anmeldung für Dragqueens & -kings',
-			body: 'Bist du Dragqueen oder -king? Möchtest du an der BernPride dabei sein? Dann melde dich mit folgendem Formular an und du wirst im März von uns kontaktiert. <a href="https://forms.gle/BcwWBJ1FV1wfpkrc9">Hier gehts zum Formular</a>'
+			body: 'Bist du Dragqueen oder -king? Möchtest du an der BernPride dabei sein? Dann melde dich mit folgendem Formular an und du wirst im März von uns kontaktiert. <a href="https://forms.gle/BcwWBJ1FV1wfpkrc9">Hier gehts zum Formular</a>.'
 		},
 		{
 			title: 'Noch Fragen?',
-			body: 'Falls Deine Fragen hier nicht beantwortet wurden, schreib uns doch einfach unter info@bernpride.ch. Wir freuen uns von Dir zu lesen!'
+			body: 'Falls Deine Fragen hier nicht beantwortet wurden, schreib uns doch einfach unter <a href="mailto:info@bernpride.ch">info@bernpride.ch</a>. Wir freuen uns von Dir zu lesen!'
 		}
 	];
 </script>
