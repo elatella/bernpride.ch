c54cdf74067c643570f2513c2e65aa565098b031
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-02-25 22:34:19 +0100

    Add schedule page

---
+++ src/routes/schedule/DailySchedule.svelte
@@ -0,0 +1,251 @@
+<script lang="ts">
+	enum Day {
+		Wednesday = 'Mittwoch',
+		Thursday = 'Donnerstag',
+		Friday = 'Freitag',
+		Saturday = 'Samstag'
+	}
+
+	enum Color {
+		Blue = 'blue',
+		Turquoise = 'turquoise',
+		Red = 'red',
+		White = 'white'
+	}
+
+	interface Activity {
+		day: Day;
+		time: string;
+		title: string;
+		description: string;
+		link?: {
+			url: string;
+			text: string;
+		};
+		location: string;
+		color: Color;
+	}
+
+	const days = [Day.Wednesday, Day.Thursday, Day.Friday, Day.Saturday];
+
+	let selectedDay: Day = Day.Saturday;
+
+	function selectDay(day: Day) {
+		selectedDay = day;
+	}
+
+	const activities: Activity[] = [
+		{
+			day: Day.Saturday,
+			time: '14:00 Uhr',
+			title: 'Eröffnungsrede Demonstration',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus quam mi, et dictum',
+			location: 'Wankdorf Center',
+			color: Color.Blue
+		},
+		{
+			day: Day.Saturday,
+			time: '14:30 Uhr',
+			title: 'Startpunkt Demonstrationsumzug',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus quam mi, et dictum',
+			link: {
+				url: '/',
+				text: 'Route'
+			},
+			location: 'Wankdorf Center',
+			color: Color.Turquoise
+		},
+		{
+			day: Day.Saturday,
+			time: '16:00 Uhr',
+			title: 'Ankunft Festivalgelände',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus quam mi, et dictum',
+			location: 'Bundesplatz',
+			color: Color.Red
+		},
+		{
+			day: Day.Saturday,
+			time: '16:30 Uhr',
+			title: 'Act XYZ',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus quam mi, et dictum',
+			link: {
+				url: 'https://open.spotify.com/artist/7M26B2dpKVQ30MPUYvzWXk',
+				text: 'Spotify'
+			},
+			location: 'Hauptbühne Bundesplatz',
+			color: Color.Turquoise
+		},
+		{
+			day: Day.Saturday,
+			time: '16:30 Uhr',
+			title: 'Irgendöppis',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus quam mi, et dictum',
+			location: 'Münsterplattform',
+			color: Color.White
+		},
+		{
+			day: Day.Saturday,
+			time: '23:30 Uhr',
+			title: 'Irgend en Party',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus quam mi, et dictum',
+			link: {
+				url: 'https://bierhuebeli.ch/',
+				text: 'Tickets'
+			},
+			location: 'Bierhübeli',
+			color: Color.Blue
+		}
+	];
+</script>
+
+<section id="daily-schedule" class="root">
+	<div class="main-content description">
+		<h2>TAGESABLAUF</h2>
+		<br />
+		<div class="buttons">
+			{#each days as day}
+				<button
+					class:selected={selectedDay === day}
+					on:click={() => {
+						selectDay(day);
+					}}
+				>
+					{day}
+				</button>
+			{/each}
+		</div>
+	</div>
+</section>
+
+<section class="activities">
+	{#each activities.filter((a) => a.day === selectedDay) as activity (activity.title)}
+		<div class="activity {activity.color.valueOf()}">
+			<div class="activity-content">
+				<p class="activity-time">{activity.time}</p>
+				<div class="activity-body">
+					<h3>{activity.title}</h3>
+					<p>{activity.description}</p>
+					{#if activity.link}
+						<a href={activity.link.url}>{activity.link.text} →</a>
+					{/if}
+				</div>
+				<p class="activity-location">{activity.location}</p>
+			</div>
+		</div>
+	{/each}
+</section>
+
+<style>
+	.root {
+		display: flex;
+		justify-content: center;
+		color: var(--red);
+	}
+
+	.main-content.description {
+		flex-direction: column;
+		margin-top: 50px;
+		margin-bottom: 50px;
+	}
+
+	.buttons {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin-top: 30px;
+	}
+
+	button {
+		margin-bottom: 15px;
+		background-color: var(--white);
+		color: var(--red);
+	}
+
+	button:hover {
+		filter: brightness(85%);
+	}
+
+	button.selected {
+		background-color: var(--red);
+		color: var(--white);
+	}
+
+	.activities {
+		display: flex;
+		flex-direction: column;
+		color: var(--red);
+	}
+
+	.activity {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		padding: 1em;
+	}
+
+	.activity-content {
+		max-width: 1024px;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.activity-time {
+		margin-bottom: 1em;
+	}
+
+	.activity-body {
+		margin-bottom: 1em;
+	}
+
+	.red {
+		background-color: var(--red);
+		color: var(--yellow);
+	}
+
+	.blue {
+		background-color: var(--blue);
+	}
+
+	.turquoise {
+		background-color: var(--turquoise);
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+
+	@media only screen and (min-width: 768px) {
+		.buttons,
+		.activity,
+		.activity-content {
+			flex-direction: row;
+		}
+
+		.activity-time {
+			width: 15%;
+		}
+
+		.activity-body {
+			width: 60%;
+		}
+
+		.activity-location {
+			width: 20%;
+			text-align: right;
+		}
+
+		button {
+			margin: 20px;
+		}
+	}
+</style>
---
+++ src/routes/schedule/Description.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import arrow from '$lib/images/arrow-right-white.svg';
+	import arrowRed from '$lib/images/arrow-right-red.svg';
+</script>
+
+<section id="description" class="root">
+	<div class="main-content">
+		<p class="body">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce finibus quam mi, et dictum
+			massa laoreet at. Integer consectetur congue neque ut luctus. Sed consectetur libero ante, vel
+			consequat lorem maximus et. In in purus posuere, semper ex ut, sagittis sapien. Sed in quam
+			augue. Quisque non condimentum risus, eu aliquam enim.
+		</p>
+		<br />
+		<div class="buttons">
+			<a href="/schedule" class="button">
+				Zum Pride-Programm
+				<img src={arrow} alt="arrow right" width="30" height="18" class="arrow" />
+			</a>
+			<a href="https://eurogames2023.ch/activities/" class="button secondary">
+				Zum EuroGames-Programm
+				<img src={arrowRed} alt="arrow right" width="30" height="18" />
+			</a>
+		</div>
+	</div>
+</section>
+
+<style>
+	.root {
+		display: flex;
+		justify-content: center;
+		background-color: var(--turquoise);
+		color: var(--red);
+	}
+
+	.main-content {
+		flex-direction: column;
+		margin-top: 50px;
+		margin-bottom: 50px;
+	}
+
+	.buttons {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-evenly;
+		margin-top: 30px;
+	}
+
+	a.button {
+		margin: 15px 0;
+		width: 100%;
+		text-decoration: none;
+		border-radius: 999px;
+		border-color: var(--red);
+		border-style: solid;
+		padding: 10px 20px 14px;
+		font-size: 80%;
+		color: var(--white);
+		background-color: var(--red);
+	}
+
+	a.button.secondary {
+		color: var(--red);
+		background-color: var(--turquoise);
+	}
+
+	a.button:hover {
+		filter: brightness(85%);
+	}
+
+	.arrow {
+		margin-left: 15px;
+	}
+
+	@media only screen and (min-width: 768px) {
+		.buttons {
+			flex-direction: row;
+		}
+
+		a.button {
+			margin: 20px;
+		}
+	}
+</style>
