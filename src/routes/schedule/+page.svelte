0e1ed124cee89d89eb99daad8016ad0890fe4141
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-02-25 22:34:19 +0100

    Add schedule page

---
+++ src/routes/schedule/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Heroine from './Heroine.svelte';
+	import Description from './Description.svelte';
+	import DailySchedule from './DailySchedule.svelte';
+</script>
+
+<svelte:head>
+	<title>BernPride - Programm</title>
+</svelte:head>
+
+<Heroine />
+
+<Description />
+
+<DailySchedule />
