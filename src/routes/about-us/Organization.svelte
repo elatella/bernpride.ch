f95dc97c5f130209462605c85b2aa446c51a39e2
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-03-12 13:41:40 +0100

    Add advisory board to about us page

--- src/routes/about-us/Organization.svelte
+++ src/routes/about-us/Organization.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import advisory from '$lib/images/advisory.webp';
+</script>
+
 <section id="description" class="root">
 	<div class="main-content">
 		<h2>Organisation</h2>
@@ -6,12 +10,15 @@
 		<p>
 			Im Jahr 2023 wird die BernPride durch den Verein EuroGames Bern organisiert, welcher nach den
 			EuroGames wieder aufgelöst wird. Eine jährliche Weiterführung der BernPride wird jedoch
-			angestrebt. Der Verein wird begleitet durch einen Beirat, in dem die Organisationen der
-			Community (LGBTI-Verbände) vereint sind. Die BernPride und der Verein Eurogames verfolgen
-			keine kommerziellen Interessen. Allfällige Gewinne werden künftigen Veranstaltungen zur
-			Verfügung gestellt, welche dieselben Ziele verfolgen, z.B. die Organisation der BernPride
-			2024.
+			angestrebt. Die BernPride und der Verein EuroGames verfolgen keine kommerziellen Interessen.
+			Allfällige Gewinne werden künftigen Veranstaltungen zur Verfügung gestellt, welche dieselben
+			Ziele verfolgen, z.B. die Organisation der BernPride 2024. Der Verein wird begleitet durch
+			einen Beirat, in dem die Organisationen der Community (LGBTI-Verbände) vereint sind.
 		</p>
+		<br />
+		<br />
+
+		<img src={advisory} alt="Beirat" width="380" height="268" />
 	</div>
 </section>
 
@@ -24,6 +31,7 @@
 
 	.main-content {
 		flex-direction: column;
+		align-items: center;
 		margin-top: 50px;
 		margin-bottom: 50px;
 	}
