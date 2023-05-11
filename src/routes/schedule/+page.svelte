8deb454eba9476dc58f4754fecb9ad616e975601
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-05-11 12:21:54 +0200

    Remove schedule description

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Heroine from './Heroine.svelte';
-	import Description from './Description.svelte';
 	import DailySchedule from './DailySchedule.svelte';
 </script>
 
@@ -9,7 +8,5 @@
 </svelte:head>
 
 <Heroine />
-
-<Description />
 
 <DailySchedule />
