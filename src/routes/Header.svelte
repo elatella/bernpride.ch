927061f9d586736f0bb51135f00d3510799ce2cd
Raphaela Seeger <elatella@users.noreply.github.com>
2023-11-25 17:29:02 +0100

    Update header and remove Photos link

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,7 +3,16 @@
 	import logo from '$lib/images/logo-yellow.svg';
 	import menu from '$lib/images/menu.svg';
 	import closeMenu from '$lib/images/closemenu.svg';
+	import instagram from '$lib/images/instagram.svg';
+	import facebook from '$lib/images/facebook.svg';
+	import tiktok from '$lib/images/tiktok.svg';
 
+	const socialNetworks: { name: string; logo: string; link: string }[] = [
+			{ name: 'Instagram', logo: instagram, link: 'https://www.instagram.com/bernpride.ch/' },
+			{ name: 'Facebook', logo: facebook, link: 'https://www.facebook.com/bernpride.ch/' },
+			{ name: 'TikTok', logo: tiktok, link: 'https://www.tiktok.com/@bernpride.ch' }
+		];
+	
 	let isNavOpen: boolean;
 </script>
 
@@ -18,6 +27,7 @@
 			<img src={logo} alt="BernPride" width="147" height="30" />
 		</a>
 
+	{#if now < end}
 		<div>
 			<input
 				type="image"
@@ -29,6 +39,15 @@
 				}}
 			/>
 		</div>
+	{:else}
+		<div>
+			{#each socialNetworks as nw (nw.name)}
+				<a href={nw.link} target="_blank" rel="noreferrer" class="social-icon">
+					<img src={nw.logo} alt={nw.name} width="30" height="30" />
+				</a>
+			{/each}
+		</div>
+	{/if}
 	</div>
 </div>
 
