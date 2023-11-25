b845d06b0caf2bc4f6335b5e79f7f4bb03faae86
Raphaela Seeger <elatella@users.noreply.github.com>
2023-11-25 17:47:25 +0100

    Remove menu, for now

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -6,13 +6,17 @@
 	import instagram from '$lib/images/instagram.svg';
 	import facebook from '$lib/images/facebook.svg';
 	import tiktok from '$lib/images/tiktok.svg';
+	import { PUBLIC_EVENT_END } from '$env/static/public';
+
+	const end = new Date(PUBLIC_EVENT_END);
+	const now = new Date();
 
 	const socialNetworks: { name: string; logo: string; link: string }[] = [
-			{ name: 'Instagram', logo: instagram, link: 'https://www.instagram.com/bernpride.ch/' },
-			{ name: 'Facebook', logo: facebook, link: 'https://www.facebook.com/bernpride.ch/' },
-			{ name: 'TikTok', logo: tiktok, link: 'https://www.tiktok.com/@bernpride.ch' }
-		];
-	
+		{ name: 'Instagram', logo: instagram, link: 'https://www.instagram.com/bernpride.ch/' },
+		{ name: 'Facebook', logo: facebook, link: 'https://www.facebook.com/bernpride.ch/' },
+		{ name: 'TikTok', logo: tiktok, link: 'https://www.tiktok.com/@bernpride.ch' }
+	];
+
 	let isNavOpen: boolean;
 </script>
 
@@ -27,27 +31,33 @@
 			<img src={logo} alt="BernPride" width="147" height="30" />
 		</a>
 
-	{#if now < end}
-		<div>
-			<input
-				type="image"
-				src={isNavOpen ? closeMenu : menu}
-				alt={isNavOpen ? 'Schliessen' : 'Menu'}
-				style="width:30px;padding:0;"
-				on:click={() => {
-					isNavOpen = !isNavOpen;
-				}}
-			/>
-		</div>
-	{:else}
-		<div>
-			{#each socialNetworks as nw (nw.name)}
-				<a href={nw.link} target="_blank" rel="noreferrer" class="social-icon">
-					<img src={nw.logo} alt={nw.name} width="30" height="30" />
-				</a>
-			{/each}
-		</div>
-	{/if}
+		{#if now < end}
+			<div>
+				<input
+					type="image"
+					src={isNavOpen ? closeMenu : menu}
+					alt={isNavOpen ? 'Schliessen' : 'Menu'}
+					style="width:30px;padding:0;"
+					on:click={() => {
+						isNavOpen = !isNavOpen;
+					}}
+				/>
+			</div>
+		{:else}
+			<div>
+				{#each socialNetworks as nw (nw.name)}
+					<a
+						href={nw.link}
+						target="_blank"
+						rel="noreferrer"
+						class="social-icon"
+						style="text-decoration:none"
+					>
+						<img src={nw.logo} alt={nw.name} width="30" height="30" />
+					</a>
+				{/each}
+			</div>
+		{/if}
 	</div>
 </div>
 
