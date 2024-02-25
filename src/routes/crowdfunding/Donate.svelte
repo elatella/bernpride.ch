03b72d3e7529b77f8da12bd02ffbd705adfd65da
tmlcbnd <158034286+tmlcbnd@users.noreply.github.com>
2024-02-25 20:04:30 +0100

    Updated Image of Flag to 154

--- src/routes/crowdfunding/Donate.svelte
+++ src/routes/crowdfunding/Donate.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import prideFlag from '$lib/images/prideflag/frame_138.png';
+	import prideFlag from '$lib/images/prideflag/frame_154.png';
 
 	onMount(async () => {
 		// @ts-expect-error Temporary solution for loading of Tamaro widget.
